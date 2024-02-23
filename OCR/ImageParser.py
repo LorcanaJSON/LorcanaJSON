ba0187ba46667d997a4859a884f29485dcf6a7a0
Didero <Didero@users.noreply.github.com>
2024-02-23 18:43:17 +0100

    Add Willpower Unicode symbol

--- OCR/ImageParser.py
+++ OCR/ImageParser.py
@@ -14,6 +14,7 @@ EXERT_UNICODE = "⟳"  # Unicode \u27F3   HTML entities &#10227;  &#x27F3;
 INK_UNICODE = "⬡"  # Unicode \u2B21  HTML entities &#11041;  &#x2B21;
 LORE_UNICODE = "◊"  # Unicode \u25CA  HTML entities &#9674;  &#x25CA;  &loz;
 STRENGTH_UNICODE = "¤"  # Unicode \u00A4  HTML entities &#164;  &#xA4;  &curren;
+WILLPOWER_UNICODE = "⛉"  # Unicode u\26C9  HTML entities: &#9929;  &#x26C9;
 TYPE_SEPARATOR_UNICODE = "•"  # Unicode \u2022 HTML entities &#8226; &bull;
 
 ImageAndText = namedtuple("ImageAndText", ("image", "text"))
