19626af1c35ba8db4d27b70f3cd09b5a5a0061dd
Didero <Didero@users.noreply.github.com>
2024-05-16 20:54:46 +0200

    Replace fancy quotemarks with basic ones in simpleName

--- DataFilesGenerator.py
+++ DataFilesGenerator.py
@@ -528,7 +528,7 @@ def _parseSingleCard(inputCard: Dict, cardType: str, imageFolder: str, enchanted
 		outputCard["fullName"] += " - " + outputCard["subtitle"]
 		outputCard["simpleName"] += " " + outputCard["subtitle"]
 	# simpleName is the full name with special characters and the base-subtitle dash removed, for easier lookup. So remove the special characters
-	outputCard["simpleName"] = re.sub(r"[!.?]", "", outputCard["simpleName"].lower()).replace("ā", "a").replace("œ", "oe").rstrip()
+	outputCard["simpleName"] = re.sub(r"[!.?]", "", outputCard["simpleName"].lower()).rstrip().replace("ā", "a").replace("œ", "oe").replace("“", "\"").replace("”", "\"")
 	_logger.debug(f"Current card name is '{outputCard['fullName']}', ID {outputCard['id']}")
 
 	outputCard["cost"] = inputCard["ink_cost"] if "ink_cost" in inputCard else int(parsedImageAndTextData["cost"].text, 10)
