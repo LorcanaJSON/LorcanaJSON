c59bcd53969212a48444e2ca4450f8fd289c420c
Didero <Didero@users.noreply.github.com>
2024-11-23 16:23:47 +0100

    Don't 'import *' in CardLayout
    This polluted autocomplete in IDEs with ImageArea fields when
    using 'CardLayout.' in other classes

--- OCR/CardLayout.py
+++ OCR/CardLayout.py
@@ -1,7 +1,7 @@
 import dataclasses
 from typing import Union
 
-from OCR.ImageArea import *
+from OCR import ImageArea as IA
 
 
 @dataclasses.dataclass
@@ -9,41 +9,41 @@ class CardLayout:
 	"""
 	Defines which Image Areas to use for various card parts
 	"""
-	name: ImageArea
-	types: ImageArea
-	textbox: ImageArea
+	name: IA.ImageArea
+	types: IA.ImageArea
+	textbox: IA.ImageArea
 	# Character only
-	version: Union[ImageArea, None] = None  # The subtitle of a card, only used for Characters and Locations
-	strength: Union[ImageArea, None] = None
-	willpower: Union[ImageArea, None] = None
+	version: Union[IA.ImageArea, None] = None  # The subtitle of a card, only used for Characters and Locations
+	strength: Union[IA.ImageArea, None] = None
+	willpower: Union[IA.ImageArea, None] = None
 	# Location only
-	moveCost: Union[ImageArea, None] = None
+	moveCost: Union[IA.ImageArea, None] = None
 	# At the bottom so we can set a default, since it's the same for almost every card
 	# Actually set this in __post_innit__ instead of just setting it here because dataclass field defaults can't be mutable
-	ink: ImageArea = None
-	artist: ImageArea = None
-	identifier: ImageArea = None
+	ink: IA.ImageArea = None
+	artist: IA.ImageArea = None
+	identifier: IA.ImageArea = None
 
 	def __post_init__(self):
 		if self.ink is None:
-			self.ink = INK_COST
+			self.ink = IA.INK_COST
 		if self.artist is None:
-			self.artist = ARTIST
+			self.artist = IA.ARTIST
 		if self.identifier is None:
-			self.identifier = CARD_IDENTIFIER
+			self.identifier = IA.CARD_IDENTIFIER
 
 
-DEFAULT = CardLayout(CARD_NAME, TYPE, FULL_WIDTH_TEXT_BOX)
-DEFAULT_CHARACTER = CardLayout(CHARACTER_NAME, TYPE, CHARACTER_TEXT_BOX, CARD_IDENTIFIER, CHARACTER_VERSION, STRENGTH, WILLPOWER)
-DEFAULT_LOCATION = CardLayout(LOCATION_NAME, LOCATION_TYPE, LOCATION_TEXTBOX, LOCATION_VERSION, willpower=LOCATION_WILLPOWER, moveCost=LOCATION_MOVE_COST, artist=LOCATION_ARTIST, identifier=LOCATION_IDENTIFIER)
+DEFAULT = CardLayout(IA.CARD_NAME, IA.TYPE, IA.FULL_WIDTH_TEXT_BOX)
+DEFAULT_CHARACTER = CardLayout(IA.CHARACTER_NAME, IA.TYPE, IA.CHARACTER_TEXT_BOX, IA.CARD_IDENTIFIER, IA.CHARACTER_VERSION, IA.STRENGTH, IA.WILLPOWER)
+DEFAULT_LOCATION = CardLayout(IA.LOCATION_NAME, IA.LOCATION_TYPE, IA.LOCATION_TEXTBOX, IA.LOCATION_VERSION, willpower=IA.LOCATION_WILLPOWER, moveCost=IA.LOCATION_MOVE_COST, artist=IA.LOCATION_ARTIST, identifier=IA.LOCATION_IDENTIFIER)
 
-ENCHANTED = dataclasses.replace(DEFAULT, textbox=ENCHANTED_FULL_WIDTH_TEXT_BOX)
-ENCHANTED_CHARACTER = dataclasses.replace(DEFAULT_CHARACTER, textbox=ENCHANTED_CHARACTER_TEXT_BOX)
-ENCHANTED_LOCATION = dataclasses.replace(DEFAULT_LOCATION, textbox=ENCHANTED_LOCATION_TEXT_BOX)
+ENCHANTED = dataclasses.replace(DEFAULT, textbox=IA.ENCHANTED_FULL_WIDTH_TEXT_BOX)
+ENCHANTED_CHARACTER = dataclasses.replace(DEFAULT_CHARACTER, textbox=IA.ENCHANTED_CHARACTER_TEXT_BOX)
+ENCHANTED_LOCATION = dataclasses.replace(DEFAULT_LOCATION, textbox=IA.ENCHANTED_LOCATION_TEXT_BOX)
 
-QUEST = dataclasses.replace(DEFAULT, textbox=QUEST_FULL_WIDTH_TEXT_BOX)
-QUEST_CHARACTER = dataclasses.replace(DEFAULT, textbox=QUEST_TEXTBOX)
+QUEST = dataclasses.replace(DEFAULT, textbox=IA.QUEST_FULL_WIDTH_TEXT_BOX)
+QUEST_CHARACTER = dataclasses.replace(DEFAULT, textbox=IA.QUEST_TEXTBOX)
 
-NEW_ENCHANTED = CardLayout(NEW_ENCHANTED_CARD_NAME, NEW_ENCHANTED_TYPE, NEW_ENCHANTED_FULL_WIDTH_TEXT_BOX)
-NEW_ENCHANTED_CHARACTER = CardLayout(NEW_ENCHANTED_CHARACTER_NAME, NEW_ENCHANTED_TYPE, NEW_ENCHANTED_CHARACTER_TEXT_BOX, NEW_ENCHANTED_VERSION, STRENGTH, WILLPOWER)
-NEW_ENCHANTED_LOCATION = dataclasses.replace(ENCHANTED_LOCATION, types=NEW_ENCHANTED_LOCATION_TYPE, textbox=NEW_ENCHANTED_LOCATION_TEXT_BOX)
+NEW_ENCHANTED = CardLayout(IA.NEW_ENCHANTED_CARD_NAME, IA.NEW_ENCHANTED_TYPE, IA.NEW_ENCHANTED_FULL_WIDTH_TEXT_BOX)
+NEW_ENCHANTED_CHARACTER = CardLayout(IA.NEW_ENCHANTED_CHARACTER_NAME, IA.NEW_ENCHANTED_TYPE, IA.NEW_ENCHANTED_CHARACTER_TEXT_BOX, IA.NEW_ENCHANTED_VERSION, IA.STRENGTH, IA.WILLPOWER)
+NEW_ENCHANTED_LOCATION = dataclasses.replace(ENCHANTED_LOCATION, types=IA.NEW_ENCHANTED_LOCATION_TYPE, textbox=IA.NEW_ENCHANTED_LOCATION_TEXT_BOX)
