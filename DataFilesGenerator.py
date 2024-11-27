fff7fc35b8331fefb127d5d3109079a30ed2b41a
Didero <Didero@users.noreply.github.com>
2024-11-27 20:37:31 +0100

    Don't use deprecated datetime.utcnow()
    Using the suggested 'now(UTC)' adds '+00:00' to the end, which is undesirable,
    so just use local time for now, it doesn't matter too much

--- DataFilesGenerator.py
+++ DataFilesGenerator.py
@@ -496,7 +496,7 @@ def createOutputFiles(onlyParseIds: Union[None, List[int]] = None, shouldShowIma
 	fullCardList.sort(key=lambda c: c["id"])
 	os.makedirs(outputFolder, exist_ok=True)
 	# Add metadata
-	metaDataDict = {"formatVersion": FORMAT_VERSION, "generatedOn": datetime.datetime.utcnow().isoformat(timespec="seconds"), "language": GlobalConfig.language.code}
+	metaDataDict = {"formatVersion": FORMAT_VERSION, "generatedOn": datetime.datetime.now().isoformat(timespec="seconds"), "language": GlobalConfig.language.code}
 	outputDict = {"metadata": metaDataDict}
 
 	# Add set data
