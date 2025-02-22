faeb9f61a26a44927b36bddd43cf639018d3c904
Didero <Didero@users.noreply.github.com>
2025-02-22 20:44:49 +0100

    Do more identifier parsing for special-case cards

--- DataFilesGenerator.py
+++ DataFilesGenerator.py
@@ -665,7 +665,7 @@ def _parseSingleCard(inputCard: Dict, cardType: str, imageFolder: str, enchanted
  		showImage=shouldShowImage
 	)
 
-	if parsedImageAndTextData.get("identifier", None) is not None:
+	if parsedImageAndTextData.get("identifier", None) is not None and (parsedImageAndTextData["identifier"].text.startswith("0") or "TFC" in parsedImageAndTextData["identifier"].text or GlobalConfig.language.uppercaseCode not in parsedImageAndTextData["identifier"].text):
 		outputCard["fullIdentifier"] = re.sub(fr" ?\W (?!$)", f" {LorcanaSymbols.SEPARATOR} ", parsedImageAndTextData["identifier"].text)
 		outputCard["fullIdentifier"] = outputCard["fullIdentifier"].replace("I", "/").replace("1P ", "/P ").replace("//", "/").replace(".", "").replace("1TFC", "1 TFC")
 		outputCard["fullIdentifier"] = re.sub(fr" ?[-+] ?", f" {LorcanaSymbols.SEPARATOR} ", outputCard["fullIdentifier"])
