20f8024feb4e50def71ae5349a273987a3c31198
Didero <Didero@users.noreply.github.com>
2025-04-02 18:53:58 +0200

    In Verifier, also compare 'maxCardsInDeck' with English

--- output/Verifier.py
+++ output/Verifier.py
@@ -218,8 +218,8 @@ def compareInputToOutput(cardIdsToVerify: Union[List[int], None]):
 		if idToEnglishOutputCard:
 			englishCard = idToEnglishOutputCard[outputCard["id"]]
 			cardId = outputCard["id"]
-			for fieldname in ('abilities', 'artistsText', 'enchantedId', 'cost', 'effects', 'fullTextSections', 'inkwell', 'keywordAbilities',
-							  'lore', 'moveCost', 'nonEnchantedId', 'nonPromoId', 'number', 'strength', 'subtypes', 'variant', 'variandIds', 'willPower'):
+			for fieldname in ('abilities', 'artistsText', 'enchantedId', 'cost', 'effects', 'fullTextSections', 'inkwell', 'keywordAbilities', 'lore',
+							  'maxCardsInDeck', 'moveCost', 'nonEnchantedId', 'nonPromoId', 'number', 'strength', 'subtypes', 'variant', 'variandIds', 'willPower'):
 				if fieldname not in outputCard and fieldname not in englishCard:
 					continue
 				if fieldname in outputCard and fieldname not in englishCard:
