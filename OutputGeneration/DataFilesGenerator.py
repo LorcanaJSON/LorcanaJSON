4c6b2cf7bf55da62bc258dbb8be32d30172290cb
Didero <Didero@users.noreply.github.com>
2025-05-07 21:10:48 +0200

    Improve 'accidentally-split' logline
    Remove the newlines from the output

--- OutputGeneration/DataFilesGenerator.py
+++ OutputGeneration/DataFilesGenerator.py
@@ -952,7 +952,7 @@ def _parseSingleCard(inputCard: Dict, cardType: str, imageFolder: str, enchanted
 			# Sometimes lines get split up into separate list entries when they shouldn't be,
 			# for instance in choice lists, or just accidentally. Fix that. But if the previous line ended with a closing bracket and this with an opening one, don't join them
 			if remainingTextLine.startswith("-") or (re.match(r"[a-z(]", remainingTextLine) and not remainingTextLines[remainingTextLineIndex-1].endswith(")")) or remainingTextLine.count(")") > remainingTextLine.count("("):
-				_logger.info(f"Merging accidentally-split line '{remainingTextLine}' with previous line '{remainingTextLines[remainingTextLineIndex - 1]}'")
+				_logger.info(f"Merging accidentally-split line {remainingTextLine!r} with previous line {remainingTextLines[remainingTextLineIndex - 1]!r}")
 				remainingTextLines[remainingTextLineIndex - 1] += "\n" + remainingTextLines.pop(remainingTextLineIndex)
 
 		for remainingTextLine in remainingTextLines:
