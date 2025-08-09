28590397f172d9856fece9a6155fb0f37da06f2f
Didero <Didero@users.noreply.github.com>
2025-08-09 19:37:04 +0200

    In label fallback, expand effect regex
    So it can handle 'G one of your characters'

--- OCR/ImageParser.py
+++ OCR/ImageParser.py
@@ -379,7 +379,7 @@ class ImageParser:
 							labelAndEffectText = remainingText[labelMatch.start():]
 							remainingText = remainingText[:labelMatch.start()].rstrip()
 							while labelAndEffectText:
-								effectMatch = re.search(fr"(([A-Z]|[ÀI|] )[a-zü]|[0-9{LorcanaSymbols.EXERT}@©&]|G ?[,—–-])", labelAndEffectText, flags=re.DOTALL)
+								effectMatch = re.search(fr"(([A-Z]|[ÀI|] )[a-zü]|[0-9{LorcanaSymbols.EXERT}@©&]|(^|\s)G( [a-z ]*)?[,—–-])", labelAndEffectText, flags=re.DOTALL)
 								if effectMatch:
 									labelText = labelAndEffectText[:effectMatch.start()]
 									effectText = labelAndEffectText[effectMatch.start():]
