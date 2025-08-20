30d8c3bc67fe101f8fe502d4b318dd2ceaaa4171
Didero <Didero@users.noreply.github.com>
2025-08-20 19:14:16 +0200

    In RelatedCardsCollator, handle Epic and Iconic rarities

--- OutputGeneration/RelatedCardsCollator.py
+++ OutputGeneration/RelatedCardsCollator.py
@@ -17,6 +17,9 @@ class RelatedCardCollator:
 		self.deckbuildingIdToRelatedCards: Dict[str, RelatedCards] = {}
 		for cardtype, cardlist in cardstore["cards"].items():
 			for card in cardlist:
+				# if GlobalConfig.language.uppercaseCode not in card["card_identifier"]:
+				# 	# Skip cards not in this language
+				# 	continue
 				groupId = card["deck_building_id"]
 				if groupId not in self.deckbuildingIdToRelatedCards:
 					self.deckbuildingIdToRelatedCards[groupId] = RelatedCards()
@@ -31,8 +34,9 @@ class RelatedCards:
 	def __init__(self):
 		self.firstNormalCardId: int = 0
 		self.normalCardIdsBySet: Dict[str, int] = {}
-		self.firstEnchantedCardId: int = 0
+		self.epicCardIdsBySet: Dict[str, int] = {}
 		self.enchantedCardIdsBySet: Dict[str, int] = {}
+		self.iconicCardIdsBySet: Dict[str, int] = {}
 		self.promoCardIds: List[int] = []
 		self.variantCardIds: List[int] = []
 		self.printedInSets: List[str] = []  # List of set codes
@@ -52,19 +56,18 @@ class RelatedCards:
 			self.variantCardIds.append(cardId)
 		if parsedIdentifier.isPromo():
 			self.promoCardIds.append(cardId)
+		elif card["rarity"] == "EPIC":
+			self._addFancyArtBySet(card, parsedIdentifier.setCode, self.epicCardIdsBySet)
 		elif card["rarity"] == "ENCHANTED":
-			if not self.firstEnchantedCardId:
-				self.firstEnchantedCarId = cardId
-			if parsedIdentifier.setCode in self.enchantedCardIdsBySet:
-				_logger.warning(f"Card {CardUtil.createCardIdentifier(card)} from set {parsedIdentifier.setCode} is Enchanted, but an Enchanted for this set is already stored, pointing to ID {self.enchantedCardIdsBySet[parsedIdentifier.setCode]}; not storing the ID")
-			else:
-				self.enchantedCardIdsBySet[parsedIdentifier.setCode] = cardId
+			self._addFancyArtBySet(card, parsedIdentifier.setCode, self.enchantedCardIdsBySet)
+		elif card["rarity"] == "ICONIC":
+			self._addFancyArtBySet(card, parsedIdentifier.setCode, self.epicCardIdsBySet)
 		elif parsedIdentifier.number > 204:
 			# A card numbered higher than the normal 204 that isn't an Enchanted is also most likely a promo card (F.i. the special Q1 cards like ID 1179)
 			self.promoCardIds.append(cardId)
 		else:
-			if not self.firstNormalCardId:
-				# Store the first non-Enchanted non-Promo card, so Enchanted and Promo cards can reference it
+			# Store the non-Enchanted non-Promo card with the lowest ID as the 'original', so Enchanted and Promo cards, and reprints, can reference it
+			if not self.firstNormalCardId or cardId < self.firstNormalCardId:
 				self.firstNormalCardId = cardId
 			# Don't store variant cards as normal cards
 			if isNotVariantCard:
@@ -72,6 +75,13 @@ class RelatedCards:
 					_logger.warning(f"Card {CardUtil.createCardIdentifier(card)} from set {parsedIdentifier.setCode} is normal, but a normal card for this set is already stored, pointing to ID {self.normalCardIdsBySet[parsedIdentifier.setCode]}; not storing the ID")
 				else:
 					self.normalCardIdsBySet[parsedIdentifier.setCode] = cardId
+
+	@staticmethod
+	def _addFancyArtBySet(card: Dict, setCode: str, fancyArtBySet: Dict[str, int]):
+		if setCode in fancyArtBySet:
+			_logger.warning(f"Card {CardUtil.createCardIdentifier(card)} from set {setCode} is rarity {card['rarity']}, but a {card['rarity']}-rarity for this set is already stored, pointing to ID {fancyArtBySet[setCode]}; not storing the ID")
+		else:
+			fancyArtBySet[setCode] = card["culture_invariant_id"]
 
 	def hasSpecialCards(self) -> bool:
 		if self.enchantedCardIdsBySet or self.promoCardIds or self.variantCardIds:
@@ -86,7 +96,7 @@ class RelatedCards:
 		"""
 		otherRelatedCards = OtherRelatedCards()
 		isNotPromo: bool = True
-		isNotEnchanted: bool = True
+		isNotFancyArt: bool = True
 		isNotVariant: bool = True
 		if self.promoCardIds:
 			if cardId in self.promoCardIds:
@@ -94,16 +104,31 @@ class RelatedCards:
 				otherRelatedCards.nonPromoId = self.firstNormalCardId
 			else:
 				otherRelatedCards.promoIds = self.promoCardIds.copy()
-		if isNotPromo and setCode in self.enchantedCardIdsBySet:
-			if cardId == self.enchantedCardIdsBySet[setCode]:
-				isNotEnchanted = False
-				otherRelatedCards.nonEnchantedId = self.firstNormalCardId
-			else:
-				otherRelatedCards.enchantedId = self.enchantedCardIdsBySet[setCode]
+
+		if isNotPromo:
+			if setCode in self.epicCardIdsBySet:
+				if cardId == self.epicCardIdsBySet[setCode]:
+					isNotFancyArt = False
+					otherRelatedCards.nonEpicId = self.firstNormalCardId
+				else:
+					otherRelatedCards.epicId = self.epicCardIdsBySet[setCode]
+			if setCode in self.enchantedCardIdsBySet:
+				if cardId == self.enchantedCardIdsBySet[setCode]:
+					isNotFancyArt = False
+					otherRelatedCards.nonEnchantedId = self.firstNormalCardId
+				else:
+					otherRelatedCards.enchantedId = self.enchantedCardIdsBySet[setCode]
+			if setCode in self.iconicCardIdsBySet:
+				if cardId == self.iconicCardIdsBySet[setCode]:
+					isNotFancyArt = False
+					otherRelatedCards.nonIconicId = self.firstNormalCardId
+				else:
+					otherRelatedCards.iconicId = self.iconicCardIdsBySet[setCode]
+
 		if self.variantCardIds and cardId in self.variantCardIds:
 			isNotVariant = False
 			otherRelatedCards.otherVariantIds = [i for i in self.variantCardIds if i != cardId]
-		if isNotPromo and isNotEnchanted and len(self.normalCardIdsBySet) > 0:
+		if isNotPromo and isNotFancyArt and len(self.normalCardIdsBySet) > 0:
 			# More than one normal card means this is or has a reprint
 			if cardId == self.firstNormalCardId:
 				otherRelatedCards.reprintedAsIds = [i for i in self.normalCardIdsBySet.values() if i != cardId]
@@ -114,8 +139,12 @@ class RelatedCards:
 
 class OtherRelatedCards:
 	def __init__(self):
+		self.epicId: Optional[int] = None
+		self.nonEpicId: Optional[int] = None
 		self.enchantedId: Optional[int] = None
 		self.nonEnchantedId: Optional[int] = None
+		self.iconicId: Optional[int] = None
+		self.nonIconicId: Optional[int] = None
 		self.promoIds: Optional[List[int]] = None
 		self.nonPromoId: Optional[int] = None
 		self.otherVariantIds: Optional[List[int]] = None
