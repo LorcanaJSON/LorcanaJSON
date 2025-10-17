1adec89538bc99358da044c1abbb7c2a5e244148
Didero <Didero@users.noreply.github.com>
2025-10-17 19:06:50 +0200

    Improve ability name junk character cleanup

--- OutputGeneration/SingeCardDataGenerator.py
+++ OutputGeneration/SingeCardDataGenerator.py
@@ -343,7 +343,7 @@ def parseSingleCard(inputCard: Dict, cardType: str, imageFolder: str, threadLoca
 		for abilityIndex in range(len(ocrResult.abilityLabels)):
 			abilityName = TextCorrection.correctPunctuation(ocrResult.abilityLabels[abilityIndex].replace("''", "'").replace("ß", "ẞ")).rstrip(" %:").upper()
 			originalAbilityName = abilityName
-			abilityName = re.sub(r"(?<=\w) ?[.;7|»”©(\"=~]$", "", abilityName)
+			abilityName = re.sub(r"(?<=\w) ?[.;7|>»”©(\"=~_]{1,2}$", "", abilityName)
 			if GlobalConfig.language == Language.ENGLISH:
 				abilityName = abilityName.replace("|", "I")
 				abilityName = re.sub("^l", "I", abilityName)
