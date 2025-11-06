7eb4c37f9bd1c610bd805fd4087c21bf2a1e5981
Didero <Didero@users.noreply.github.com>
2025-11-06 19:10:31 +0100

    Add parse settings for new promos
    Offset is needed because there's a tiny black
    vertical line on the left otherwise that throws
    off the label finder

--- OCR/ParseSettings.py
+++ OCR/ParseSettings.py
@@ -146,6 +146,8 @@ _PARSE_SETTINGS_BY_ID: Dict[int, ParseSettings] = {
 	2443: dataclasses.replace(_PARSE_SETTINGS_BY_GROUPING["P3"], labelIsDarkerThanBackground=False, labelTextColor=ImageArea.TEXT_COLOUR_BLACK, labelMaskColor=_BLACK, thresholdTextColor=ImageArea.TEXT_COLOUR_WHITE),
 	2444: _PARSE_SETTINGS_FOR_ENCHANTED_BY_SET["10"],
 	2445: _PARSE_SETTINGS_FOR_ENCHANTED_BY_SET["10"],
+	2461: dataclasses.replace(_PARSE_SETTINGS_BY_GROUPING["P3"], labelStartThreshold=100, labelEndThreshold=170, labelTextColor=ImageArea.TEXT_COLOUR_MIDDLE, textboxOffset=10),
+	2462: dataclasses.replace(_PARSE_SETTINGS_BY_GROUPING["P3"], labelStartThreshold=100, labelEndThreshold=170, labelTextColor=ImageArea.TEXT_COLOUR_MIDDLE, textboxOffset=10),
 }
 
 def getParseSetingsById(cardId: int) -> Optional[ParseSettings]:
