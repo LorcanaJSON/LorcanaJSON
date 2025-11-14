dc8aa97bad1c72e75bf28fc0b24d5711679f7382
Didero <Didero@users.noreply.github.com>
2025-11-14 19:18:00 +0100

    Strip more junk from start of ability names

--- OutputGeneration/SingleCardDataGenerator.py
+++ OutputGeneration/SingleCardDataGenerator.py
@@ -343,9 +343,8 @@ def parseSingleCard(inputCard: Dict, cardType: str, imageFolder: str, threadLoca
 
 	if ocrResult.abilityLabels:
 		for abilityIndex in range(len(ocrResult.abilityLabels)):
-			abilityName = TextCorrection.correctPunctuation(ocrResult.abilityLabels[abilityIndex].replace("''", "'").replace("ß", "ẞ")).rstrip(" %:").upper()
+			abilityName = TextCorrection.correctPunctuation(ocrResult.abilityLabels[abilityIndex].replace("''", "'").replace("ß", "ẞ")).lstrip("-+_.… ").rstrip(" %:").upper()
 			originalAbilityName = abilityName
-			abilityName = re.sub("^[-_… ]+", "", abilityName)
 			abilityName = re.sub(r"(?<=\w) ?[.;7|>»”©(\"=~_]{1,2}$", "", abilityName)
 			if GlobalConfig.language == Language.ENGLISH:
 				abilityName = abilityName.replace("|", "I")
