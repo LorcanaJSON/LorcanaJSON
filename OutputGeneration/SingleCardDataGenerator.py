48df36c9acbe5fe465dea5a2bf5c0b2f8fb556f6
Didero <Didero@users.noreply.github.com>
2025-12-06 14:20:29 +0100

    In English abilityname, correct "ITLL" to "IT'LL"

--- OutputGeneration/SingleCardDataGenerator.py
+++ OutputGeneration/SingleCardDataGenerator.py
@@ -353,6 +353,7 @@ def parseSingleCard(inputCard: Dict, cardType: str, imageFolder: str, threadLoca
 				abilityName = re.sub("^l", "I", abilityName)
 				abilityName = re.sub(r"\bLM\b", "I'M", abilityName)
 				abilityName = re.sub(r"(^|\s)['’P]?LL\b", "\\1I'LL", abilityName)
+				abilityName = abilityName.replace("ITLL", "IT'LL")
 				# Use simple quotemark for plural possesive
 				abilityName = re.sub(r"(?<=[A-Z]S)’(?=\s)", "'", abilityName)
 			elif GlobalConfig.language == Language.FRENCH:
