72b8f797fa14153deac4ccd850f9dce60d92a6d7
Didero <Didero@users.noreply.github.com>
2026-04-17 20:25:22 +0200

    Improve ellipsis correction in abilityname check

--- OutputGeneration/SingleCardDataGenerator.py
+++ OutputGeneration/SingleCardDataGenerator.py
@@ -281,7 +281,7 @@ def parseSingleCard(inputCard: Dict, ocrResult: OcrResult, externalLinksHandler:
 				# First create a list of ability labels in the input text, if we haven't done that already
 				if not inputAbilityNames:
 					# Ability names are listed between '\' in titlecase, we need them upper case because that's how it's written on the card
-					inputAbilityNames: List[str] = [s.upper().replace(". . . ", "...") for s in re.findall(r"\\([^\\]+)\\", inputCard["rules_text"])]
+					inputAbilityNames: List[str] = [re.sub(r" ?\.\s\.\s\.\s?", "...", s.upper()) for s in re.findall(r"\\([^\\]+)\\", inputCard["rules_text"])]
 				if abilityIndex >= len(inputAbilityNames):
 					_logger.error(f"Trying to read input ability name index {abilityIndex} but there are only {len(inputAbilityNames)} names, in card {CardUtil.createCardIdentifier(outputCard)}")
 				else:
