3675bc1327b6da6004735db931d129ff5916c871
Didero <Didero@users.noreply.github.com>
2026-07-20 13:03:22 +0200

    Save dated input data file in subfolder
    Having all the input JSON files in one folder
    got far too cluttered
    
    There is no migration code, so existing files will
    have to be manually moved to a 'year/languageCode' subfolder

--- APIScraping/ApiScrapingUtil.py
+++ APIScraping/ApiScrapingUtil.py
@@ -10,6 +10,9 @@ def saveCardCatalog(cardCatalog: Dict, shouldSaveDatedFile: bool = True):
 	with open(os.path.join(pathToSaveTo, f"carddata.{GlobalConfig.language.code}.json"), "w", encoding="utf-8") as cardfile:
 		json.dump(cardCatalog, cardfile, indent=2)
 	if shouldSaveDatedFile:
-		datedCardCatalogPath = os.path.join(pathToSaveTo, f"carddata.{GlobalConfig.language.code}.{datetime.datetime.now().strftime('%Y-%m-%d.%H-%M-%S')}.json")
+		now = datetime.datetime.now()
+		pathToSaveTo = os.path.join(pathToSaveTo, str(now.year), GlobalConfig.language.code)
+		os.makedirs(pathToSaveTo, exist_ok=True)
+		datedCardCatalogPath = os.path.join(pathToSaveTo, f"carddata.{GlobalConfig.language.code}.{now.strftime('%Y-%m-%d.%H-%M-%S')}.json")
 		with open(datedCardCatalogPath, "w", encoding="utf-8") as cardfile:
 			json.dump(cardCatalog, cardfile, indent=2)
