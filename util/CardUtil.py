852f36afcc347284715c958da5333624840949a0
Didero <Didero@users.noreply.github.com>
2026-07-24 20:03:35 +0200

    In card name parse method, add way to print promogrouping

--- util/CardUtil.py
+++ util/CardUtil.py
@@ -1,13 +1,18 @@
 from typing import Dict
 
-def createCardIdentifier(card: Dict) -> str:
+def createCardIdentifier(card: Dict, addIdentiferNumberGroup: bool = False) -> str:
 	"""
 	Create an identifier string for an input or output card, consisting of the full name and the ID
 	:param card: The card dictionary
+	:param addIdentiferNumberGroup Whether to add the first part of the identifier to the output (f.i. '3/204', '1/P1'). If not provided, defaults to False
 	:return: A string with the full card name and the card ID
 	"""
 	if "id" in card:
 		# Output card
-		return f"'{card['fullName']}' (ID {card['id']})"
+		result = f"'{card['fullName']}' (ID {card['id']}"
+		if addIdentiferNumberGroup:
+			result += f", {card['fullIdentifier'].split(' ', 1)[0]}"
+		result += ")"
+		return result
 	# Input card
 	return f"'{card['name']} - {card.get('subtitle', None)}' (ID {card['culture_invariant_id']})"
